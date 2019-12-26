b2bb623b9f183019beba16499720b5dc55d5b7a4
andreas <andreasfabrinberglund@gmail.com>
2019-12-26 22:40:42 +0100

    simplest

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-#app
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Small beginnings'))
+
+app.listen(port, () => console.log(`Dosmer app listening on port ${port}!`))
