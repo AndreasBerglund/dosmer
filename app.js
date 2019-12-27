4d9077f565306e88d3a5212facf54d42884535f8
andreas <andreasfabrinberglund@gmail.com>
2019-12-27 11:57:51 +0100

    shortid

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cookieParser = require('cookie-parser')
+const shortid = require('shortid');
 
-app.get('/', (req, res) => res.send('Small beginnings'))
+ 
+app.use(cookieParser());
+
+
+app.get('/make', (req, res) => { 
+    // read cookies
+    console.log(req.cookies) 
+    res.cookie('dosmerlist', shortid.generate())
+    res.send('Made a list')
+});
 
 app.listen(port, () => console.log(`Dosmer app listening on port ${port}!`))
