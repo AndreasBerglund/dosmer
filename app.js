21b6ec21f4d15ef82f535c0994b33407bc97e733
andreas <andreasfabrinberglund@gmail.com>
2019-12-28 15:35:40 +0100

    database

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ const shortid = require('shortid');
  
 app.use(cookieParser());
 
+//db
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 
 app.get('/make', (req, res) => { 
     // read cookies
